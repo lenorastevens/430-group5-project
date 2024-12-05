91a38dc6b5b5aff23e4b0c4ad1c17d4731add7c1
Lenora Stevens <lenoraRStevens@gmail.com>
2024-12-04 22:16:36 -0600

    added the product details page loading dynamically

---
+++ src/interfaces/Product.ts
@@ -0,0 +1,11 @@
+export interface Product {
+    product_id: number;
+    product_name: string;
+    product_description: string;
+    product_price: number;
+    product_image: string;
+    artisan_id: number;
+    artisan_firstname: string;
+    artisan_lastname: string;
+    category_id: number;
+  }
