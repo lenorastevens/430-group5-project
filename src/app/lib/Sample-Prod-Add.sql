38fa85eb141e0b18efedfb9d2eb3ab33971df2db
Lenora Stevens <lenoraRStevens@gmail.com>
2024-12-08 17:59:48 -0600

    sql file update

--- src/app/lib/Sample-Prod-Add.sql
+++ src/app/lib/Sample-Prod-Add.sql
@@ -3,24 +3,14 @@ INSERT INTO product (
     product_description, 
     product_price, 
     product_image, 
-    product_thumbnail, 
     category_id, 
     artisan_id) 
-    VALUES (
-        'Glass Box', 
-        'This stylish glass trunk box with a brass frame serves as an organizer for your several items of daily use.',
-        '61.25',
-        'glass-box.webp',
-        'glass_box.webp',
-        2,
-        4
-        ),
+    VALUES 
         (
             'Stylish Bracelet',
             'This stylish gold bracelet is the way to go for a special event and it gives you the confidence.',
             '14.67',
             'Bracelet.webp',
-            '',
             3,
-            2
+            3
         );
